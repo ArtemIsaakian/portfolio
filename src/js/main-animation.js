c2c707113691fa4082477b4132516263e3747e10
Artem Isaakyan <artem.issakyan@raketa.agency>
2022-11-30 12:57:01 +0400

    added "see below" sign

--- src/js/main-animation.js
+++ src/js/main-animation.js
@@ -17,25 +17,15 @@ function hideDescription(el) {
 
 function show() {
   arr.some((el,i) => {
-    setTimeout(showDescription, 1800 * (++i), el);
+    setTimeout(showDescription, 1500 * (++i), el);
   })
 }
 
 function hide() {
   arr.some((el,i) => {
-    setTimeout(hideDescription, 1800 * (++i + 0.5), el);
+    setTimeout(hideDescription, 2000 * (++i + 0.5), el);
   })
 }
 
-//== для первого запуска автоматического показа карточек ==//
-
-let firstAutoplayShow = setInterval(show, 1000)
-let firstAutoplayHide = setInterval(hide, 1000)
-
-setTimeout(() => clearInterval(firstAutoplayShow), 1200)
-setTimeout(() => clearInterval(firstAutoplayHide), 1200)
-
-//== для последующего автоматического показа карточек ==//
-
-let autoplayShow = setInterval(show, 6000)
-let autoplayPlayHide = setInterval(hide, 6000)
+show()
+hide()
